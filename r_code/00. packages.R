62721db7f8a0021574624974cf1fe14b3042445a
Linda Tempel <36073785+LindaTempel@users.noreply.github.com>
2018-06-11 14:22:17 +0200

    Add files via upload

---
+++ r_code/00. packages.R
@@ -0,0 +1,16 @@
+# Load necessary packages
+
+
+
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("visreg")
+install.packages("multcomp")
+install.packages("effects")
+
+
+require(plyr)
+require(dplyr)
+require(multcomp)
+require(visreg)
+require(effects)
